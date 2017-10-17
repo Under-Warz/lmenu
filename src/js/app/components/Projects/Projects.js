2721e0885ec62bbb186445ce3bcc28ac4fba326b
Laurent Menu <menu.laurent@gmail.com>
2017-10-17 18:06:05 +0200

    Integration projects

--- src/js/app/components/Projects/Projects.js
+++ src/js/app/components/Projects/Projects.js
@@ -1,6 +1,9 @@
 // import vendors
 import React from 'react';
-import PropTypes from 'prop-types';
+import i18next from 'i18next';
+
+// import classes
+import Button from '../Button';
 
 // import styles
 import styles from './styles';
@@ -9,10 +12,21 @@ import styles from './styles';
 class Projects extends React.Component {
 
   //________________________________________________________
-  // -                                            PROP TYPES
-  static propTypes = {
-  };
+  // -                                       PRIVATE METHODS
 
+  _renderProjects = () => {
+    return i18next.t('projects.projects', { returnObjects: true }).map((project, index) => {
+      return <div key={index} className="project">
+        <div className="picture"><img src={require('assets/images/projects/' + project.image)} /></div>
+        <div className="details">
+          <h4>{project.name}</h4>
+          <ul><li><span><i></i>{project.subtitle}</span></li></ul>
+          <p>{project.description}</p>
+          <Button href="#">{project.cta}</Button>
+        </div>
+      </div>;
+    });
+  }
 
   //________________________________________________________
   // -                                        PUBLIC METHODS
@@ -24,7 +38,16 @@ class Projects extends React.Component {
   render () {
     return (
       <section className={styles.projects}>
+        <div className="container">
+          <div className="section-title">
+            <h3>{i18next.t('projects.title')}</h3>
+            <p dangerouslySetInnerHTML={{__html: i18next.t('projects.subtitle')}} />
+          </div>
 
+          <div className="projects">
+            {this._renderProjects()}
+          </div>
+        </div>
       </section>
     );
   }
