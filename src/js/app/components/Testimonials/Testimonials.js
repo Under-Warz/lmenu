2b84455a3645125c9c6e2bcb8e46e1c699539cdd
Laurent Menu <menu.laurent@gmail.com>
2017-10-19 11:36:38 +0200

    finalize testimonials

--- src/js/app/components/Testimonials/Testimonials.js
+++ src/js/app/components/Testimonials/Testimonials.js
@@ -8,15 +8,23 @@ import styles from './styles';
 
 class Testimonials extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: 0
+    };
+  }
+
   //________________________________________________________
   // -                                       PRIVATE METHODS
 
   _renderTestimonials = () => {
     return i18next.t('testimonials', { returnObjects: true }).map((testimonial, index) => {
-      return <div key={index} className="testimonial">
+      return <div key={index} className={["testimonial", (index === this.state.active ? "active" : null)].join(' ')}>
         <blockquote>“{testimonial.message}“</blockquote>
         <div className="meta">
-          <span className="thumb">
+          <span className="thumb" style={testimonial.thumb ? { backgroundImage: 'url(' + testimonial.thumb + ')' } : { backgroundColor: testimonial.color }}>
             {testimonial.thumb == null && testimonial.initials}
           </span>
           <span className="function">{testimonial.function}</span>
@@ -27,10 +35,30 @@ class Testimonials extends React.Component {
 
   //________________________________________________________
   // -                                        PUBLIC METHODS
-  shouldComponentUpdate () {
-    return false;
+  componentDidMount() {
+    const length = i18next.t('testimonials', { returnObjects: true }).length;
+
+    // Start timer
+    this.interval = setInterval(() => {
+      if (this.state.active == length - 1) {
+        this.setState({
+          active: 0
+        });
+      }
+      else {
+        this.setState({
+          active: this.state.active + 1
+        });
+      }
+    }, 4000);
   }
 
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  }
 
   render () {
     return (
