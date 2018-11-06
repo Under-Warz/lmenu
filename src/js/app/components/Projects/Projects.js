b909a2554a5a5627a50a299b215d21ce199fc376
Laurent Menu <hello@lmenu.fr>
2018-11-06 15:23:29 +0100

    update content

--- src/js/app/components/Projects/Projects.js
+++ src/js/app/components/Projects/Projects.js
@@ -1,61 +1,86 @@
 // import vendors
-import React from 'react';
-import i18next from 'i18next';
+import React from "react";
+import i18next from "i18next";
 
 // import classes
-import Button from '../Button';
+import Button from "../Button";
 
 // import styles
-import styles from './styles';
-
+import styles from "./styles";
 
 class Projects extends React.Component {
-
   //________________________________________________________
   // -                                       PRIVATE METHODS
 
   _renderProjects = () => {
-    return i18next.t('projects.projects', { returnObjects: true }).map((project, index) => {
-      return <div key={index} className="project">
-        <div className="picture"><img src={require('assets/images/projects/' + project.image)} /></div>
-        <div className="details">
-          <h4>{project.name}</h4>
-          <ul><li><span><i></i>{project.subtitle}</span></li></ul>
-          <p dangerouslySetInnerHTML={{__html: project.description}} />
-          <Button href={project.link} onClick={(e) => this._onClickProject(project.name)} target="_blank">{project.cta}</Button>
-        </div>
-      </div>;
-    });
-  }
+    return i18next
+      .t("projects.projects", { returnObjects: true })
+      .map((project, index) => {
+        return (
+          <div key={index} className="project">
+            <div className="picture">
+              <img
+                src={require("assets/images/projects/" + project.image)}
+                srcSet={
+                  project.imageRetina
+                    ? `${require("assets/images/projects/" +
+                        project.imageRetina)} 2x`
+                    : null
+                }
+              />
+            </div>
+            <div className="details">
+              <h4>{project.name}</h4>
+              <ul>
+                <li>
+                  <span>
+                    <i />
+                    {project.subtitle}
+                  </span>
+                </li>
+              </ul>
+              <p dangerouslySetInnerHTML={{ __html: project.description }} />
+              <Button
+                href={project.link}
+                onClick={e => this._onClickProject(project.name)}
+                target="_blank"
+              >
+                {project.cta}
+              </Button>
+            </div>
+          </div>
+        );
+      });
+  };
 
-  _onClickProject = (name) => {
+  _onClickProject = name => {
     // Track event
-    ga('send', 'event', 'Projects', 'click', name);
-  }
+    ga("send", "event", "Projects", "click", name);
+  };
 
   //________________________________________________________
   // -                                        PUBLIC METHODS
-  shouldComponentUpdate () {
+  shouldComponentUpdate() {
     return false;
   }
 
-
-  render () {
+  render() {
     return (
       <section className={styles.projects}>
         <div className="container">
           <div className="section-title">
-            <h3>{i18next.t('projects.title')}</h3>
-            <p dangerouslySetInnerHTML={{__html: i18next.t('projects.subtitle')}} />
+            <h3>{i18next.t("projects.title")}</h3>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: i18next.t("projects.subtitle")
+              }}
+            />
           </div>
 
-          <div className="projects">
-            {this._renderProjects()}
-          </div>
+          <div className="projects">{this._renderProjects()}</div>
         </div>
       </section>
     );
   }
-
 }
 export default Projects;
